1844c320840d08ad0305cb73a484a3e0de35a4cb
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-01 14:28:33 +0300

    init

--- ScrollViewIntroduction/ContentView.swift
+++ ScrollViewIntroduction/ContentView.swift
@@ -9,8 +9,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello World")
+        TabView {
+            // Tab 1
+            ZStack {
+                Color.blue.colorMultiply(.red).edgesIgnoringSafeArea(.top)
+                VStack(spacing: 20) {
+                    Text("TabView")
+                        .font(.largeTitle).foregroundColor(.white)
+                    Text("TabItem Colors")
+                        .font(.title).foregroundColor(.gray)
+                    Text("Set the color of the active tab item by setting the accent color for the TabView.")
+        }
+            }.tabItem {
+                Image(systemName: "star.fill")
+        }
+        // Tab 2
+            Text("Second Screen").tabItem {
+                Image(systemName: "moon.fill")
+            }.foregroundColor(Color.red)
+            // Tab 3
+            Text("Third Screen").tabItem {
+                Image(systemName: "sun.min.fill")
+            }
+        }
+        .edgesIgnoringSafeArea(.top)
+        .accentColor(.yellow)
     }
 }
 
---
+++ ScrollViewIntroduction/DealView.swift
@@ -0,0 +1,21 @@
+//
+//  DealView.swift
+//  ScrollViewIntroduction
+//
+//  Created by ramil on 01/10/2019.
+//  Copyright © 2019 com.ri. All rights reserved.
+//
+
+import SwiftUI
+
+struct DealView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DealView_Previews: PreviewProvider {
+    static var previews: some View {
+        DealView()
+    }
+}
